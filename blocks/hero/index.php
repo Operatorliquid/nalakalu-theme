c2e20ae8df90e9b60fbfe701db3f89f6ec16740f
Alfredo Gago <alfredo@5e.cr>
2025-10-02 13:59:17 -0600

    commit with tailwind

--- blocks/hero/index.php
+++ blocks/hero/index.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Hero Block render callback
  * Using ACF fields for content
  */
 
 // Make sure ACF is active
-if ( ! function_exists('get_field') ) {
+if (! function_exists('get_field')) {
     echo '<p><em>ACF plugin required.</em></p>';
     return;
 }
@@ -19,15 +20,15 @@ $desc       = get_field('description');
 $bg_url = $background && isset($background['url']) ? esc_url($background['url']) : '';
 
 ?>
-<section class="hero-block alignfull" style="background-image:url('<?php echo $bg_url; ?>')">
-    <div class="hero-block__overlay"></div>
-    <div class="hero-block__content">
+<section class="hero-block alignfull relative min-h-[60vh] bg-cover bg-center flex items-center justify-center text-white text-center" style="background-image:url('<?php echo $bg_url; ?>')">
+    <div class="absolute inset-0 bg-black bg-opacity-40"></div>
+    <div class="relative z-10 max-w-4xl px-8">
         <?php if ($title): ?>
-            <h1 class="hero-block__title"><?php echo esc_html($title); ?></h1>
+            <h1 class="text-5xl font-bold mb-4"><?php echo esc_html($title); ?></h1>
         <?php endif; ?>
 
         <?php if ($desc): ?>
-            <p class="hero-block__desc"><?php echo esc_html($desc); ?></p>
+            <p class="text-xl leading-relaxed"><?php echo esc_html($desc); ?></p>
         <?php endif; ?>
     </div>
 </section>
